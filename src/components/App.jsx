7cf1697eb1e6e862fba33243d2b57bf7ae6cb507
chizh273 <chizh273@gmail.com>
2017-12-14 18:53:58 +0200

    Added simple routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
 import React, { Fragment } from 'react'
 import Header from './shared/Header/Header.jsx'
+import Home from '@/components/pages/Home/Home'
 import NewsItemPage from '@/components/pages/News-item-page/News-item-page'
+import { Switch, Route, Router } from 'react-router'
+import createBrowserHistory from 'history/createBrowserHistory'
+
+const history = createBrowserHistory()
 
 function App () {
   return (
-    <Fragment>
+    <Fragment >
       <Header>
         {'Hacker news clone'}
       </Header>
 
-      <NewsItemPage />
-
+      <Router history={history}>
+        <Switch>
+          <Route component={Home} extact path="/" />
+          <Route component={NewsItemPage} path="/item/:id" />
+        </Switch>
+      </Router>
     </Fragment>
   )
 }
